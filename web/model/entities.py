da45699ecaa2c7fe928c155ed3372e2d9f10ebe2
Fabrizio Vásquez <fabrizio.vasquez@utec.edu.pe>
2019-05-21 18:52:13 -0500

    first commit

--- web/model/entities.py
+++ web/model/entities.py
@@ -2,9 +2,19 @@ from sqlalchemy import Column, Integer, String, Sequence, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 from database import connector
 
-class Book(connector.Manager.Base):
-    __tablename__ = 'users'
+#class Book(connector.Manager.Base):
+#    __tablename__ = 'users'
+#    id = Column(Integer, Sequence('book_id_seq'), primary_key=True)
+#    name = Column(String(50))
+#    isbn = Column(String(12))
+#    title = Column(String(120))
+
+
+class Formato(connector.Manager.Base):
+    __tablename__='users'
     id = Column(Integer, Sequence('book_id_seq'), primary_key=True)
-    name = Column(String(50))
-    isbn = Column(String(12))
-    title = Column(String(120))
+    Codigo = Column(String(9))
+    Nombre = Column(String(100))
+    Apellido = Column(String(20))
+    Password = Column(String(20))
+
--- web/server.py
+++ web/server.py
@@ -7,26 +7,45 @@ db = connector.Manager()
 engine = db.createEngine()
 
 app = Flask(__name__)
+app.debug = True
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/books')
+@app.route('/forms')
 def users():
     db_session = db.getSession(engine)
-    users = db_session.query(entities.Book)
+    users = db_session.query(entities.Formato)
     data = users[:]
+    #db_session1 =db.getSession(engine)
+    #users1= db_session1.query(entities.Forms)
+    #data1 = users1[:]
+
+
     return Response(json.dumps(data, cls=connector.AlchemyEncoder), mimetype = 'application/json')
 
-@app.route('/create_test_books', methods = ['GET'])
-def create_test_books():
+#@app.route('/create_test_books', methods = ['GET'])
+#def create_test_books():
+    #db_session = db.getSession(engine)
+    #book = entities.Book(name="HOLAAA", isbn="12345", title="HOLA HTML5")
+    #db_session.add(book)
+    #db_session.commit()
+
+
+@app.route('/create_test_formate_tarea')
+def create_test_formate():
     db_session = db.getSession(engine)
-    book = entities.Book(name="Head First HTML5", isbn="12345", title="Head first about HTML5")
-    db_session.add(book)
+    formate = entities.Formato(Codigo="201907833",Nombre="Renzo",Apellido="Bustamante",Password="AARM&X86AMD12")
+    db_session.add(formate)
     db_session.commit()
-    return "Test books created!"
+    return "Test Dictionary created!"
+
+
+
+
 
 if __name__ == '__main__':
     app.secret_key = ".."
